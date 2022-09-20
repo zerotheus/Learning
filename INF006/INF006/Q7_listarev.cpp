f912123cf903caac3a11e89d9b42e685fcb999e6
AlanaSann <109287005+AlanaSann@users.noreply.github.com>
2022-09-20 20:04:01 -0300

    Update Q7_listarev.cpp
    Printa a matriz e maior menor corrigido

--- INF006/INF006/Q7_listarev.cpp
+++ INF006/INF006/Q7_listarev.cpp
@@ -6,32 +6,46 @@ using namespace std;
 
 int main()
 {
-  int n = 5, l, c, i;
+  int n = 3, l, c, i;
   int matriz[n][n], arrmaior[n], arrmenor[n];
-
+  int maior = 0, menor = 0;
   for (l = 0; l < n; l++)
   {
     for (c = 0; c < n; c++)
     {
       printf("digite l:%d c:%d\n", l + 1, c + 1);
       scanf("%d", &matriz[l][c]);
-      if (c == 0)
+      if (l == 0)
       {
-        arrmaior[l] = matriz[l][c];
-        arrmenor[l] = matriz[l][c];
+        arrmaior[c] = matriz[l][c];
+        arrmenor[c] = matriz[l][c];
       }
-      if (arrmaior[l] < matriz[l][c])
+      if (arrmaior[c] < matriz[l][c])
       {
-        arrmaior[l] = matriz[l][c];
+        arrmaior[c] = matriz[l][c];
       }
-      if (arrmenor[l] > matriz[l][c])
+      if (arrmenor[c] > matriz[l][c])
       {
-        arrmenor[l] = matriz[l][c];
+        arrmenor[c] = matriz[l][c];
       }
     }
   }
+  for(i = 0; i < n; i++){
+      
+      maior += arrmaior[i];
+      menor += arrmenor[i];
+  }
+   for (l = 0; l < n; l++)
+  {
+    for (c = 0; c < n; c++)
+    {
+      printf("%d", matriz[l][c]);
+    }
+      printf("printf\n");
+  }
   for (i = 0; i < n; i++)
   {
-    printf("maior: %d, menor: %d, a+b = %d\n", arrmaior[i], arrmenor[i], arrmaior[i] + arrmenor[i]);
+    printf("maior: %d, menor: %d, a+b = %d\n", arrmaior[i], arrmenor[i], (maior + menor)/2);
   }
+    
 }
